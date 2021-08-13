456d3c6361df4d15c81c6bbe97af46661ba38562
edveloso <edveloso@gmail.com>
2021-08-13 19:29:11 -0300

    change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bem vindo ao meu pipeline!'));
+app.get('/', (req, res) => res.send('Bem vindo ao meu pipeline mains uma mudança!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
